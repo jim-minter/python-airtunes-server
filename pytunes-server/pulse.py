c431bd73144da5e12b04c7b69bf04b150cb284bf
Jim Minter <jminter@redhat.com>
2019-04-09 12:31:16 -0500

    set pulse buffer

--- pytunes-server/pulse.py
+++ pytunes-server/pulse.py
@@ -10,6 +10,13 @@ class pa_sample_spec(ctypes.Structure):
                 ('channels', ctypes.c_uint8)]
 
 
+class pa_buffer_attr(ctypes.Structure):
+    _fields_ = [('maxlength', ctypes.c_uint32),
+                ('tlength', ctypes.c_uint32),
+                ('prebuf', ctypes.c_uint32),
+                ('minreq', ctypes.c_uint32),
+                ('fragsize', ctypes.c_uint32)]
+
 def get_next_frame():
     data = ctypes.create_string_buffer(352 * 2 * 2)
     libpulse.pa_simple_read(ctypes.c_void_p(pcm), data, 352 * 2 * 2, None)
@@ -26,8 +33,9 @@ def init(stream):
     global pcm
 
     pcm = libpulse.pa_simple_new(None, "pytunes-server", 2, stream, "monitor",
-                                 ctypes.byref(pa_sample_spec(3, 44100, 2)),
-                                 None, None, None)
+                                 ctypes.byref(pa_sample_spec(3, 44100, 2)), None,
+                                 ctypes.byref(pa_buffer_attr(-1, -1, -1, -1, 352 * 2 * 2)),
+                                 None)
 
 
 def deinit():
